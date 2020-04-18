afd1eae2bbf66d2025e2c0b89cf2f770296c1398
FireSwordGame <FireSwordGame@users.noreply.github.com>
2020-04-18 12:22:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sergiorojo_ejercicioeva_rec/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sergiorojo_ejercicioeva_rec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
